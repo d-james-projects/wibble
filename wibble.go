07d85738c48a2f1083573b7563543abfad703cc6
d-james-projects <you@example.com>
2020-03-03 19:18:43 +0000

    updates

--- wibble.go
+++ wibble.go
@@ -5,6 +5,13 @@ import (
 	"net"
 )
 
+type Proto struct {
+	a   string
+	b   net.IP
+	c   int
+	txt string
+}
+
 type Pip struct {
 	x   int
 	y   int
@@ -18,27 +25,49 @@ func NewPip(t string) *Pip {
 	return &p
 }
 
-type TheRealClient struct{}
+type RealClient struct{}
 
 type MyClientServices interface {
-	Dial(n string, addr string) (net.Conn, error)
-	Close()
+	Dial(n, addr string) (net.Conn, error)
+	//	Close(*net.Conn)
 }
 
 type MyClient struct {
-	m MyClientServices
+	mClientServices MyClientServices
 }
 
-func myclient(pip Pip, dest string) error {
+func (r RealClient) Dial(n string, addr string) (net.Conn, error) {
+	fmt.Printf("Call Dial() for type %T\n", r)
+	conn, err := net.Dial(n, addr)
+
+	return conn, err
+}
+
+func (r RealClient) Close(conn *net.Conn) {
+	fmt.Printf("Call Close() for type %T\n", r)
+
+	_ = (*conn).Close()
+}
+
+func (r RealClient) PipToProto(pip Pip) *Proto {
+	fmt.Printf("Call PipToProto() for type %T\n", r)
+	p := Proto{}
+	p.txt = "hello world"
+	p.c = 0
+	return &p
+}
+
+func (c MyClient) startService(pip Pip, dest string) error {
 	//Connect udp
-	conn, err := net.Dial("udp", dest)
+	//conn, err := net.Dial("udp", dest)
+	conn, err := c.mClientServices.Dial("udp", dest)
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
+	//defer c.mClientServices.Close(&conn)
 
 	//simple write
-	//conn.Write([]byte("Hello from client"))
+	conn.Write([]byte("Hello from client"))
 	//todo PipToProto(pip).send(conn)
 
 	return nil
@@ -49,7 +78,11 @@ func main() {
 	pip := Pip{1, 2, "terrier"}
 
 	fmt.Printf("[-] connecting to port > %s\n", p)
-	err := myclient(pip, p)
+	//err := myclient(pip, p)
+	c := RealClient{}
+	srvc := MyClient{c}
+	err := srvc.startService(pip, p)
+
 	if err != nil {
 		panic(err)
 	}
--- wibble_test.go
+++ wibble_test.go
@@ -1,15 +1,61 @@
 package main
 
 import (
-	"log"
+	"errors"
+	"fmt"
 	"net"
-	"os"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
+type mockClient struct {
+	mock.Mock
+}
+
+/*
+func (m *mockClient) Close(conn *net.Conn) {
+	fmt.Printf("Call Close() for type %T\n", *m)
+	_ = m.Called(conn)
+}
+*/
+func (m *mockClient) Dial(n string, addr string) (net.Conn, error) {
+	fmt.Printf("Call Dial() for type %T\n", *m)
+	args := m.Called(n, addr)
+	return args.Get(0).(net.Conn), args.Error(1)
+}
+
+func TestStartService(t *testing.T) {
+	p := "127.0.0.1:6789"
+	pip := Pip{1, 2, "terrier"}
+	c := RealClient{}
+	srvc := MyClient{c}
+
+	assert.Nil(t, srvc.startService(pip, p))
+
+	assert.NotNil(t, srvc.startService(pip, "wibble"))
+}
+
+func TestStartServiceMocked(t *testing.T) {
+	pip := Pip{1, 2, "terrier"}
+	//test := new(net.Conn)
+	obj, _ := net.Pipe()
+	defer obj.Close()
+
+	c := new(mockClient)
+
+	//c.On("Dial", "udp", "127.0.0.1:6789").Return(obj, nil).Once()
+	c.On("Dial", "udp", "wobble").Return(obj, errors.New("bad net")).Once()
+
+	srvc := MyClient{c}
+	srvc.startService(pip, "wobble")
+
+	c.AssertExpectations(t)
+
+}
+
+/*
 func TestMyclient(t *testing.T) {
 
 	assert.Nil(t, myclient("127.0.0.1:6789"))
@@ -59,3 +105,4 @@ func TestMyclientWithConn(t *testing.T) {
 	r := <-done
 	assert.Equal(t, 17, r, "should be equal")
 }
+*/
